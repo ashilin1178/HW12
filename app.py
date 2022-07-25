76c5bc4404ec6d0dd1c61590c778f4a879ace643
Anton <ashilin1178@gmail.com>
2022-07-25 17:03:41 +0800

    проект готов

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ from flask import Flask
 from loader.loader import loader_blueprint
 from main.main import main_blueprint
 
-# from functions import ...
-
-# POST_PATH = "posts.json"
-# UPLOAD_FOLDER = "uploads/images"
-
 app = Flask(__name__)
 
 logging.basicConfig(filename='log.txt', level=logging.INFO, encoding='utf-8')
